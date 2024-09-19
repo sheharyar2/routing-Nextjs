47f6075df71d5a7885f83eb530f5a901be6052b4
Sheharyar Javed <sheharyarj@gmail.com>
2024-09-19 13:34:40 +0500

    add custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1>ERROR 404</h1>
+        <h1>PAGE NOT FOUND</h1>
+    </div>
+
+  )
+}
+
+export default NotFound
