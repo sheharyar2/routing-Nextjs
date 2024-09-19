f8ceeacae4b9fa37b7364363a0275b21790a4120
Sheharyar Javed <sheharyarj@gmail.com>
2024-09-19 18:14:41 +0500

    Nested Layouts

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const DashboardLayout = ({children}:
+    {
+        children:React.ReactNode
+    }
+) => {
+  return (
+    <div>
+        <h1>
+            Dashboard
+        </h1>
+        {children}
+    </div>
+  )
+}
+
+export default DashboardLayout
